12e05a25c11610a17441aeb2db30604f6838a2e1
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-10-23 23:18:48 +0700

    final report : comparison sequential & parallel in convolution

---
+++ conv_sequential.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <omp.h> 
+
+int main(){
+    // ---- input and malloc A, F ----
+    int NA, NF;
+    scanf("%d %d", &NA, &NF);
+    int *A = malloc(sizeof(int) * NA);
+    int *F = malloc(sizeof(int) * NF);
+
+    for(int i = 0; i < NA; i++){
+        scanf("%d", &A[i]);
+    }
+    for(int i = 0; i < NF; i++){
+        scanf("%d", &F[i]);
+    }
+    // ---- end input and malloc----
+
+    // implement here
+    // FF initialize creating reverse array of F
+    int *FF = malloc(sizeof(int) *NF) ; 
+    for ( int i = 0 ; i < NF ; i++) { 
+        FF[i] = F[NF-i-1] ; 
+    }
+    // printf("FF reverse list :  ")  ; 
+    // for (int i = 0 ; i < NF ; i++){
+    //     printf("%d ", FF[i]) ;
+    // }
+
+    // Convolution logic implementation
+    // Initialize R as a result array
+    int *R = malloc(sizeof(int) * (NA - NF + 1)) ;
+    double start_time = omp_get_wtime();
+    
+    for (int i = 0; i <= NA - NF; i++) {
+        R[i] = 0; 
+        for (int j = 0; j < NF; j++) {
+            R[i] += A[i + j] * FF[j]; 
+        }
+    }
+    double end_time = omp_get_wtime();
+    printf("Sequential execution time: %f seconds\n", end_time - start_time);
+
+
+    for (int i = 0 ; i <= NA - NF ; i++){
+        printf("%d\n", R[i]) ;
+    }
+    // ---- free memory ----
+    free(FF);
+    free(F);
+    free(A);
+    free(R);
+    // ---- end free ----
+    return 0;
+}
+
